3b5b3133288d3282b8ff70a59e51fbe522f6c7d6
JordanPlayz158 <JordanAdamsBusiness@outlook.com>
2025-11-28 14:21:58 -0500

    Damn inconsistent tab sizes between IDE and GitHub

--- Fabric/src/main/java/net/puffish/skillsoriginsmod/origins/UnlockCategoryPower.java
+++ Fabric/src/main/java/net/puffish/skillsoriginsmod/origins/UnlockCategoryPower.java
@@ -13,40 +13,40 @@ import net.puffish.skillsmod.api.SkillsAPI;
 import net.puffish.skillsoriginsmod.SkillsOriginsMod;
 
 public class UnlockCategoryPower extends Power {
-			public static final Identifier ID = SkillsOriginsMod.createIdentifier("unlock_category");
+	public static final Identifier ID = SkillsOriginsMod.createIdentifier("unlock_category");
 
-			private final Identifier category;
+	private final Identifier category;
 
-			private UnlockCategoryPower(PowerType<?> type, LivingEntity entity, Identifier category) {
-				super(type, entity);
-				this.category = category;
-			}
+	private UnlockCategoryPower(PowerType<?> type, LivingEntity entity, Identifier category) {
+		super(type, entity);
+		this.category = category;
+	}
 
-			public static PowerFactory<UnlockCategoryPower> createFactory() {
-				return new PowerFactory<>(
-						ID,
-						new SerializableData().add("category", SerializableDataTypes.IDENTIFIER),
-						data -> (type, player) -> new UnlockCategoryPower(type, player, data.get("category"))
-				);
-			}
+	public static PowerFactory<UnlockCategoryPower> createFactory() {
+		return new PowerFactory<>(
+				ID,
+				new SerializableData().add("category", SerializableDataTypes.IDENTIFIER),
+				data -> (type, player) -> new UnlockCategoryPower(type, player, data.get("category"))
+		);
+	}
 
-			@Override
-			public void onAdded() {
-				super.onAdded();
-				if (entity instanceof ServerPlayerEntity player) {
-					player.server.send(new ServerTask(0, () ->
-							SkillsAPI.getCategory(category)
-									.ifPresent(category -> category.unlock(player))));
-				}
-			}
+	@Override
+	public void onAdded() {
+		super.onAdded();
+		if (entity instanceof ServerPlayerEntity player) {
+			player.server.send(new ServerTask(0, () ->
+					SkillsAPI.getCategory(category)
+							.ifPresent(category -> category.unlock(player))));
+		}
+	}
 
-			@Override
-			public void onRemoved() {
-				super.onRemoved();
-				if (entity instanceof ServerPlayerEntity player) {
-					player.server.send(new ServerTask(0, () ->
-							SkillsAPI.getCategory(category)
-									.ifPresent(category -> category.lock(player))));
-				}
-			}
+	@Override
+	public void onRemoved() {
+		super.onRemoved();
+		if (entity instanceof ServerPlayerEntity player) {
+			player.server.send(new ServerTask(0, () ->
+					SkillsAPI.getCategory(category)
+							.ifPresent(category -> category.lock(player))));
+		}
+	}
 }
